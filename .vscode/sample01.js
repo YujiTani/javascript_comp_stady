7b9fc0fd2d60211744fa222a3dd033d4583b740d
yuzunosk55 <yuzuha3ds@gmail.com>
2020-12-04 18:06:01 +0900

    1st commit

---
+++ .vscode/sample01.js
@@ -0,0 +1,7 @@
+const age = 28;
+
+// age >= 18 ? console.log("一緒にお酒を呑みましょう。") : console.log("一緒にお水を呑みましょう。");
+
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const x = '23';
+
+const fast = bathday => 2037 - bathday;
+console.log();
